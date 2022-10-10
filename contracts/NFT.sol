8e7e4a59470a4709d1f3aced958e11174da7fca2
Joseph Kouroma <jkouroma@gmail.com>
2022-10-10 18:32:33 +0100

    using openzepplin wizard

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,7 +1,29 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.8.4;
 
-contract NFT {
-  constructor() public {
-  }
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Planets is ERC721, ERC721URIStorage, Ownable {
+    constructor() ERC721("Planets", "PLN") {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://ipfs.io/ipfs/";
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
 }
